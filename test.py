18cb76c43f8e296688dc134d7ff63ff0e11b347b
NguyenMinhNghia02 <nghia.nm204594@sis.hust.edu.vn>
2023-11-29 15:50:44 +0700

    test

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-sdokasndoasd
+def generate_output_path():
+    output_path = "/path/to/your/output/file.txt"
+    print(output_path)
+    return output_path
+
+if __name__ == "__main__":
+    generate_output_path()
